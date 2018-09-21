190807542f1ddf9149b1e79a00dab9d0f0139887
Jordi Miquel Oliver Buades <jordi.oliver@logitravelgroup.com>
2018-09-21 13:21:54 +0200

    Added sample project

---
+++ samples/SampleDiagnosedApi/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SampleDiagnosedApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _factory;
+
+        public ValuesController(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            // Everytime we call this controller, http client metrics must be available in
+            // http://<host>:9303/metrics
+            var client = _factory.CreateClient("JsonPlaceholder");
+            var response = await client.GetAsync("/posts").ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }        
+    }
+}
---
+++ samples/SampleDiagnosedApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Bluekiri.Diagnostics.Prometheus;
+using Prometheus;
+
+namespace SampleDiagnosedApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Creating the kestrel metrics server listening
+            // into another port
+            var metricsServer = new KestrelMetricServer(9303);
+            metricsServer.Start();
+
+            // Subscribe the diagnostic listeners that will
+            // export Prometheus metrics
+            DiagnosticListener.AllListeners
+                .SubscribeDiagnosticListener();
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
