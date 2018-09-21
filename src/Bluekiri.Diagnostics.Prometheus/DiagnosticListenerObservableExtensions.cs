995f9236fcaddc3fe9f49b7d7491e4bb37fa69bc
Jordi Miquel Oliver Buades <jordi.oliver@logitravelgroup.com>
2018-09-21 13:21:45 +0200

    Added extensions for IObservable<DiagnosticListener>

---
+++ src/Bluekiri.Diagnostics.Prometheus/DiagnosticListenerObservableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Bluekiri.Diagnostics.Prometheus
+{
+    public static class DiagnosticListenerObservableExtensions
+    {
+
+        public static void SubscribeDiagnosticListener(this IObservable<DiagnosticListener> observable)
+        {
+            observable.Subscribe(delegate (DiagnosticListener listener)
+            {
+                if (listener.Name == "HttpHandlerDiagnosticListener")
+                {
+                    listener.Subscribe(new HttpHandlerDiagnosticListenerObserver());
+                }
+            });
+        }
+    }
+}
