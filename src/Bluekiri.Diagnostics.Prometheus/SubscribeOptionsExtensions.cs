60e1dfba7e2bf27b58ef5961c16f82d3d1ecc7cd
Jordi Oliver <jordi.obuades@gmail.com>
2018-11-02 09:55:55 +0100

    Added possibility to add custom observers for specific Diagnostic Listeners

---
+++ src/Bluekiri.Diagnostics.Prometheus/SubscribeOptionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Bluekiri.Diagnostics.Prometheus
+{
+    public static class SubscribeOptionsExtensions
+    {
+        /// <summary>
+        /// Adds the default observers
+        /// </summary>
+        /// <param name="o"></param>
+        public static void AddDefaultObservers(this SubscribeOptions o)
+        {
+            o.AddSubscriber("HttpHandlerDiagnosticListener", new HttpHandlerDiagnosticListenerObserver());
+            o.AddSubscriber("Microsoft.AspNetCore", new AspNetCoreDiagnosticListenerObserver());
+        }
+    }
+}
